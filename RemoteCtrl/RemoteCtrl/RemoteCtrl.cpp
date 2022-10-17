ff7f6abde72c657df547dd4268366e73060c4e94
薛嘉杰 <3180875763@qq.com>
2022-10-17 12:49:38 +0800

    完成了《获取指定目录下的文件和文件夹》功能

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -5,6 +5,7 @@
 #include "framework.h"
 #include "RemoteCtrl.h"
 #include "ServerSocket.h"
+#include <direct.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -19,6 +20,92 @@
 CWinApp theApp;
 
 using namespace std;
+
+void Dump(BYTE* pData, size_t nSize) {
+	std::string strOut;
+	for (size_t i = 0; i < nSize; i++) {
+		char buf[8] = "";
+		if (i > 0 && (i % 16 == 0)) {
+			strOut += "\n";
+		}
+		snprintf(buf, sizeof(buf), "%02X ", pData[i]& 0xFF);
+		strOut += buf;
+	}
+	strOut += "\n";
+	OutputDebugStringA(strOut.c_str());
+}
+
+int MakeDriverInfo() {//1==>A 2==>B 3==>C ... 26==>Z
+	std::string result;
+	for (int i = 0; i <= 26; i++) {
+		if (_chdrive(i) == 0) {
+			if (result.size() > 0) {
+				result += ',';
+			}
+			result += 'A' + i - 1;
+		}
+	}
+	CPacket pack(1, (BYTE*)result.c_str(), result.size());
+	Dump((BYTE*)pack.Data(), pack.size());
+	//CServerSocket::getInstance()->Send(pack);
+	return 0;
+}
+#include<stdio.h>
+#include <io.h>
+#include <list>
+typedef struct file_info{
+	file_info() {
+		IsInvalid = FALSE;
+		IsDirectory = -1;
+		HasNext = TRUE;
+		memset(szFileName, 0, sizeof(szFileName));
+	}
+	BOOL IsInvalid;//是否有效
+	BOOL IsDirectory;//是否为目录 0 否 1 是
+	BOOL HasNext;//是否还有后续 0 没有 1 有
+	char szFileName[256];//文件名
+}FILEINFO,*PFILEINFO;
+
+int MakeDirectcryInfo() {
+	std::string strPath;
+	//std::list<FILEINFO> lstFileInfos;
+	if (CServerSocket::getInstance()->GetFilePath(strPath) == false) {
+		OutputDebugString(_T("当前的命令不是获取文件列表，命令解析错误！"));
+		return -1;
+	}
+	if (_chdir(strPath.c_str())!=0) {
+		FILEINFO finfo;
+		finfo.IsInvalid = TRUE;
+		finfo.IsDirectory = TRUE;
+		finfo.HasNext = FALSE;
+		memcpy(finfo.szFileName, strPath.c_str(), strPath.size());
+		//lstFileInfos.push_back(finfo);
+		CPacket pack(2,(BYTE*)&finfo, sizeof(finfo));
+		CServerSocket::getInstance()->Send(pack);
+		OutputDebugString(_T("没有权限，访问目录！"));
+		return -2;
+	}
+	_finddata_t fdata;
+	int hfind = 0;
+	if ((hfind=_findfirst("*", &fdata)) == -1) {
+		OutputDebugString(_T("没有找到任何文件！"));
+		return -3;
+	}
+	do {
+		FILEINFO finfo;
+		finfo.IsDirectory = (fdata.attrib & _A_SUBDIR) != 0;
+		memcpy(finfo.szFileName, fdata.name, strlen(fdata.name));
+		//lstFileInfos.push_back(finfo);
+		CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+		CServerSocket::getInstance()->Send(pack);
+	} while (!_findnext(hfind, &fdata));
+	//发送信息到控制端
+	FILEINFO finfo;
+	finfo.HasNext = FALSE;
+	CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+	CServerSocket::getInstance()->Send(pack);
+	return 0;
+}
 
 int main()
 {
@@ -36,28 +123,39 @@ int main()
 			nRetCode = 1;
 		}
 		else
-		{//1 进度的可控性 2 对接的方便性 3 可行性评估，提高暴露风险
+		{
+			//1 进度的可控性 2 对接的方便性 3 可行性评估，提高暴露风险
 			// TODO: socket,bind,listen,accept,read,write,close
 			//套接字初始化
-			CServerSocket* pserver = CServerSocket::getInstance();
-			int count = 0;
-			if (pserver->InitSocket() == false) {
-				MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败!"), MB_OK | MB_ICONERROR);
-				exit(0);
-			}
-			while (CServerSocket::getInstance() != NULL) {
-				if (pserver->AcceptClient() == false) {
-					if (count >= 3) {
-						MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-						exit(0);
-					}
-					MessageBox(NULL, _T("无法自动接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-					count++;
-				}
-				int ret = pserver->DealCommand();
-				//TODO:
-			}
+			//CServerSocket* pserver = CServerSocket::getInstance();
+			//int count = 0;
+			//if (pserver->InitSocket() == false) {
+			//	MessageBox(NULL, _T("网络初始化异常，未能成功初始化，请检查网络状态！"), _T("网络初始化失败!"), MB_OK | MB_ICONERROR);
+			//	exit(0);
+			//}
+			//while (CServerSocket::getInstance() != NULL) {
+			//	if (pserver->AcceptClient() == false) {
+			//		if (count >= 3) {
+			//			MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+			//			exit(0);
+			//		}
+			//		MessageBox(NULL, _T("无法自动接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+			//		count++;
+			//	}
+			//	int ret = pserver->DealCommand();
+			//	//TODO:
+			//}
 			//全局的静态变量
+			int nCmd = 1;
+			switch (nCmd) {
+			case 1://查看磁盘分区
+				MakeDriverInfo();
+				break;
+			case 2://查看指定目录下的文件
+				MakeDirectcryInfo();
+				break;
+			}
+			
 		}
 	}
 	else

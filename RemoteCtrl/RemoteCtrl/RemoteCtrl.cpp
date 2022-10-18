73c9076d302c37824d1bc28b9aca4b9d7b54459e
薛嘉杰 <3180875763@qq.com>
2022-10-18 20:31:07 +0800

    屏幕截屏和发送功能

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -6,6 +6,7 @@
 #include "RemoteCtrl.h"
 #include "ServerSocket.h"
 #include <direct.h>
+#include <atlimage.h>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -235,6 +236,45 @@ int MouseEvent() {
 	return 0;
 }
 
+int SendScreen() {
+	CImage screen;//GDI
+	HDC hScreen = ::GetDC(NULL);
+	int nBitPerPixel = GetDeviceCaps(hScreen, BITSPIXEL);//24  ARGB8888 32bit RGB888 24bit RGB565 RGB444
+	int nWidth = GetDeviceCaps(hScreen, HORZRES);//宽
+	int nHeight = GetDeviceCaps(hScreen, VERTRES);//高
+	screen.Create(nWidth, nHeight, nBitPerPixel);
+	BitBlt(screen.GetDC(), 0, 0, 1920, 1020, hScreen, 0, 0, SRCCOPY);
+	ReleaseDC(NULL, hScreen);
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, 0);
+	if (hMem == NULL)return -1;
+	IStream* pStream = NULL;
+	HRESULT ret = CreateStreamOnHGlobal(hMem, TRUE, &pStream);
+	if (ret == S_OK) {
+		screen.Save(pStream, Gdiplus::ImageFormatPNG);
+		LARGE_INTEGER bg = { 0 };
+		pStream->Seek(bg, STREAM_SEEK_SET, NULL);
+		PBYTE pData=(PBYTE)GlobalLock(hMem);
+		SIZE_T nSize = GlobalSize(hMem);
+		CPacket pack(6, pData, nSize);
+		CServerSocket::getInstance()->Send(pack);
+		GlobalUnlock(hMem);
+	}
+	//screen.Save(_T("test2020.png"), Gdiplus::ImageFormatPNG);
+	//TRACE("png %d\r\n", GetTickCount64() - tick);
+	//for (int i = 0; i < 10; i++) {
+	//	DWORD tick = GetTickCount64();
+	//	screen.Save(_T("test2020.png"), Gdiplus::ImageFormatPNG);
+	//	TRACE("png %d\r\n", GetTickCount64() - tick);
+	//	tick = GetTickCount64();
+	//	screen.Save(_T("test2020.jpg"), Gdiplus::ImageFormatJPEG);
+	//	TRACE("jpg %d\r\n", GetTickCount64() - tick);
+	//}
+	pStream->Release();
+	GlobalFree(hMem);
+	screen.ReleaseDC();
+	return 0;
+}
+
 int main()
 {
 	int nRetCode = 0;
@@ -274,7 +314,7 @@ int main()
 			//	//TODO:
 			//}
 			//全局的静态变量
-			int nCmd = 1;
+			int nCmd = 6;
 			switch (nCmd) {
 			case 1://查看磁盘分区
 				MakeDriverInfo();
@@ -290,6 +330,9 @@ int main()
 				break;
 			case 5://鼠标操作
 				MouseEvent();
+				break;
+			case 6://发送屏幕内容==>发送屏幕的截图
+				SendScreen();
 				break;
 			}
 

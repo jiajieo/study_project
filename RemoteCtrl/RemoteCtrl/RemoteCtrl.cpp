dee9b60e70171fe4b977cea48ec422778cf1789a
薛嘉杰 <3180875763@qq.com>
2022-12-20 15:29:42 +0800

    1.修复远程控制坐标的bug 2.搭建了远程测试环境 3.修复了监视界面反复打开奔溃的bug 4.修复了监视界面按回车键关闭的bug

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -237,7 +237,7 @@ int SendScreen() {
 	int nWidth = GetDeviceCaps(hScreen, HORZRES);//宽
 	int nHeight = GetDeviceCaps(hScreen, VERTRES);//高
 	screen.Create(nWidth, nHeight, nBitPerPixel);
-	BitBlt(screen.GetDC(), 0, 0, 1920, 1020, hScreen, 0, 0, SRCCOPY);
+	BitBlt(screen.GetDC(), 0, 0, nWidth, nHeight, hScreen, 0, 0, SRCCOPY);
 	ReleaseDC(NULL, hScreen);
 	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, 0);
 	if (hMem == NULL)return -1;
